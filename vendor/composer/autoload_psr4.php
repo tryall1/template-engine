b10dc96795358dcf7b0ccfaf485fcb96c8534bae
Iliya <genial712@gmail.com>
2015-06-27 17:42:50 +0300

    Перегенерируем авто-загрузчик

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Project\\' => array($baseDir . '/src'),
+    'TemplateEngine\\' => array($baseDir . '/src'),
 );
