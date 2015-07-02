9e449d6a3304a7867816eced240a3e7c5a6d2c87
Iliya <genial712@gmail.com>
2015-07-02 12:51:37 +0300

    add (array=data[]) and new Exception

--- src/Template.php
+++ src/Template.php
@@ -1,22 +1,23 @@
 <?php
 namespace TemplateEngine;
 
-class Template {
-	private $vars=array();
+class Template
+{
+    private $vars = array();
+    private $debug = true;
 
-	public function __set($name, $value){
-		$this->vars[$name]=$value;
-	}
 
-	public function __get($name){
-		return $this->vars[$name];
-	}
-
-	public function render($temp){
-		extract($vars); //импортируем переменные из массива в текущую таблицу символов
-		ob_start(); //включение буферизации
-		include("$temp"); 
-		return ob_get_clean(); //получаем содержимое текущего буфера
-	}
+    public function render($temp, array $data = [])
+    {
+        if (!file_exists("$temp") && $this->$debug == true) {
+            throw new \Exception('Невозможно открыть файл'); //перед Exception стоит "\" потому что мы создаём этот объект класса в глобальном пространстве имён
+        } else {
+            extract($data); //импортируем переменные из массива в текущую таблицу символов
+            ob_start(); //включение буферизации
+            include("$temp");
+            return ob_get_clean(); //получаем содержимое текущего буфера
+        }
+    }
 }
+
 ?>
